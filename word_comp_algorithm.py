ba6e5bf840de576a61dd2acbb57cf83cb9f255ca
Emily Shull <114122607+ewshull@users.noreply.github.com>
2024-02-21 11:12:06 -0500

    Algorithm files
    aggregate_cves.py - collects dataset from MITRE CVE API. Stores in a file as a JSON payload (list of maps, where maps contain the CVE Id and Description).
    word_comp_algorithm.py - loops over the data set and compares each CVE to each other CVE. Decreasing time complexity, because as the algorithm runs it excludes CVEs that were previously checked.

---
+++ word_comp_algorithm.py
@@ -0,0 +1,94 @@
+import json
+
+"""
+!!!!!BEFORE RUNNING THIS SCRIPT, REMOVE THE LAST COMMA FROM THE LAST CVE!!!!!
+"""
+
+
+# Inspiration Source: ChatGPT
+
+
+def compare_cves_from_map(map_of_cves):
+    similarity_map = {}
+    checked_keys = set()
+
+    # Now you can iterate over each key-value pair
+    for key, value in map_of_cves.items():
+        for comparison_key, comparison_value in map_of_cves.items():
+            # do not run comparison on the same key, or if the key combination has already been checked
+            if key != comparison_key and (key, comparison_key) not in checked_keys and (
+                    comparison_key, key) not in checked_keys:
+                checked_keys.add((key, comparison_key))
+
+                a = set(value.split())
+                b = set(comparison_value.split())
+                c = a.intersection(b)
+
+                percent_similarity = float(len(c)) / (len(a) + len(b) - len(c))
+
+                if percent_similarity >= tolerance:
+                    similarity_key = f"{key}//{comparison_key}"
+                    similarity_map[similarity_key] = percent_similarity
+
+                    print(f"Checking for similarities of {similarity_key}...   Found significant similarities.")
+                else:
+                    print(f"Checking for similarities of {key}//{comparison_key}...   Not similar enough.")
+
+                print(similarity_map)
+
+    print('COMPLETE')
+
+
+def compare_cves_from_list(list_of_cves, starting_position):
+    similarity_list = []
+
+    # Now you can iterate over the list by index
+    for i in range(starting_position, len(list_of_cves)):
+        # this will only have 1 key-value pair, so not computationally intensive
+        cve_map = list_of_cves[i]
+        key = list(cve_map.keys())[0]
+        value = list(cve_map.values())[0]
+
+        # loop through all subsequent CVEs - this works because it is assumed that you have already checked
+        # the current CVE against all previous CVEs in previous loops
+        # this will be more computationally intensive upfront, and then become less intensive as the processing
+        # moves farther through the list
+        for i2 in range(i + 1, len(list_of_cves)):
+            cve_map2 = list_of_cves[i2]
+            comparison_key = list(cve_map2.keys())[0]
+            comparison_value = list(cve_map2.values())[0]
+
+            a = set(value.split())
+            b = set(comparison_value.split())
+            c = a.intersection(b)
+
+            percent_similarity = float(len(c)) / (len(a) + len(b) - len(c))
+
+            if percent_similarity >= tolerance:
+                similarity_key = f"{key}//{comparison_key}"
+                similarity_list.append((similarity_key, percent_similarity))
+
+                print(f"Checking for similarities of {similarity_key}...   Found significant similarities.")
+            else:
+                print(f"Checking for similarities of {key}//{comparison_key}...   Not similar enough.")
+
+            print(similarity_list)
+
+        print('COMPLETE')
+
+
+tolerance = 0.75  # update value with preferred tolerance level
+
+file_name = "clean_cves_list_2014_2024.json"
+
+# cleansed_cves_file = open(file_name, 'r')
+# json_data = json.load(cleansed_cves_file)
+# cleansed_cves_file.close()
+#
+# compare_cves_from_map(json_data)
+
+cleansed_cves_file = open(file_name, 'r')
+json_data = json.load(cleansed_cves_file)
+cleansed_cves_file.close()
+
+compare_cves_from_list(json_data, 164)
